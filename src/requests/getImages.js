13eff3592ad7a4fa84b991356ef6918896713611
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-03-08 14:45:59 +0000

    Installed axios and created the getImages file to handle the request

---
+++ src/requests/getImages.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const getImages = (query) => {
+  if (!query) {
+    return Promise.resolve([]);
+  }
+  return axios
+    .get(`https://images-api.nasa.gov/search?q=${query}`)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export default getImages;
