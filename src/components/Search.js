24b7f5cca32b1fcedc381289b9f1b37d1158f7e4
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-03-08 14:38:12 +0000

    Added button to and useState Hook to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 import '../styles/Search.css';
 
 const Search = () => {
+  const [value, setValue] = useState();
+
   return (
     <>
-      <input className="search-input" type="text" />
+      <form className="search-form">
+        <input
+          className="search-input"
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="search-btn" type="submit">
+          Go!
+        </button>
+      </form>
     </>
   );
 };
