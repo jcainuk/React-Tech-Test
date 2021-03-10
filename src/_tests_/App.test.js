35d23acbc9475222c7ebbe3f7442226b8272751b
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-03-10 15:49:15 +0000

    Updated tests for App to also check for nasa logo

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders correctly', () => {
-  const { asFragment } = render(<App />);
+describe('<App />', () => {
+  it('renders and matches snapshot', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-  expect(asFragment()).toMatchSnapshot();
+  it('renders as expected', () => {
+    const { getByTestId } = render(<App />);
+    const app = getByTestId('app');
+    expect(app).toBeTruthy();
+  });
+
+  it('renders logo', () => {
+    const { getByTestId } = render(<App />);
+    const logo = getByTestId('logo');
+    expect(logo).toBeTruthy();
+  });
 });
--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,12 @@ import SearchResults from './SearchResults';
 function App() {
   const [searchResults, setSearchResults] = useState();
   return (
-    <div className="App">
+    <div className="App" data-testid="app">
       <img
         className="nasa-logo"
         src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
         alt="nasa-logo"
+        data-testid="logo"
       />
       <Search setSearchResults={setSearchResults} />
       <div className="search-results">
