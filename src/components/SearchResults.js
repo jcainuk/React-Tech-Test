1a87f4d876ea52763026ce0b1e7da0cacb7f5a82
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-03-10 14:08:16 +0000

    Changed logic of results array so that it takes the prop, maps over it and takes the src of each element in  results  and adds it inside img

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,14 +6,11 @@ const SearchResults = ({ results }) => {
   if (!results.length) {
     return <p>No results</p>;
   }
-
   return (
     <>
-      <p>Search Results</p>
-      <img
-        src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        alt="tbc"
-      />
+      {results.map((image) => (
+        <img className="card-image" src={image} alt="spaceImage" />
+      ))}
     </>
   );
 };
