ebcc81f37ea8b63727a5c48d0fd0110bb8b35963
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-03-10 13:45:48 +0000

    Passed 'results' as a parameter to const SearchResults and tried to implement some conditional logic

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,20 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/SearchResults.css';
 
-const SearchResults = () => {
+const SearchResults = ({ results }) => {
+  if (!results.length) {
+    return <p>No results</p>;
+  }
+
   return (
     <>
       <p>Search Results</p>
       <img
-        className="card-image"
         src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        alt="space"
+        alt="tbc"
       />
     </>
   );
 };
 
 SearchResults.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
   results: PropTypes.array.isRequired,
 };
 
